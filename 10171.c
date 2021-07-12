1bf5c802cc0533d12bdd2e4eff136d8c20d3c0d8
3tPepper <51518828+3tPepper@users.noreply.github.com>
2021-07-13 03:45:02 +0900

    세번째

---
+++ 10171.c
@@ -0,0 +1,11 @@
+//고양이를 출력한다.
+#include <stdio.h>
+
+int main(void) {
+    printf("\\    /\\\n");
+    printf(" \)  \( \'\)\n");
+    printf("\(  /  \)\n");
+    printf(" \\\(__\)|\n");
+
+    return 0;
+}
