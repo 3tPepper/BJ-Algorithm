8163f73746f10a9b959657eb60aef66167dcc15c
3tPepper <51518828+3tPepper@users.noreply.github.com>
2021-07-13 22:50:02 +0900

    다섯번째

--- 1000.c
+++ 1000.c
@@ -4,7 +4,7 @@
 
 int main(void) {
 	int a = 0, b = 0;
-	scanf_s("%d %d", &a, &b);    //visual studio 2019를 사용하기 때문에 scanf_s 사용하였음
+	scanf_s("%d %d", &a, &b);    //visual studio 2019를 사용하기 때문에 scanf_s 사용하였습니다.
 	if (a > 0 && a < 10 && b>0 && b < 10) {
 		printf("%d", a + b);
 	}
